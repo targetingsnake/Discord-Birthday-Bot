ad64fff751cbd964d72cb44fdfacd6ed055af139
Targetingsnake <targetingsnake@users.noreply.github.com>
2025-12-24 20:06:48 +0100

    first Version for testing

--- Birthday-Bot-Sourcecode/Common/config.cs
+++ Birthday-Bot-Sourcecode/Common/config.cs
@@ -8,7 +8,7 @@ namespace Common.Cfg
 {
     public class config
     {
-        public config(string sQlPassword, string sQlUser, string sQlServer, string sQLSchema, string discordToken, IList<ulong> DcMaster, string[] _BirthdayWhishes)
+        public config(string sQlPassword, string sQlUser, string sQlServer, string sQLSchema, string discordToken, IList<ulong> DcMaster, string[] _BirthdayWhishes, string[] _BirthdayWishesAge, int _debug)
         {
             SQlPassword = sQlPassword;
             SQlUser = sQlUser;
@@ -22,15 +22,19 @@ namespace Common.Cfg
             }
             MasterDiscord = dcm;
             BirthdayWhishes = _BirthdayWhishes;
+            BirthdayWishesAge = _BirthdayWishesAge;
+            debug = (_debug == 1);
         }
 
         public string SQlPassword { get; init; }
-        public string SQLSchema { get; set; }
+        public string SQLSchema { get; init; }
         public string SQlUser { get; init; }
         public string SQlServer { get; init; }
         public string DiscordToken { get; init; }
         public ulong[] MasterDiscord { get; init; }
         public string[] BirthdayWhishes { get; init; }
+        public string[] BirthdayWishesAge { get; init; }
+        public bool debug { get; init; }
     }
 
     
