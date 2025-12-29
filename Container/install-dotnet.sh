7f1678de6d7938e75a77c688be0327e270cc460e
Targetingsnake <targetingsnake@users.noreply.github.com>
2025-12-29 20:42:00 +0100

    added file to build docker container

---
+++ Container/install-dotnet.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$(dpkg -l | awk '/microsoft/ {print }'|wc -l)" -ge 1 ]; then
+  exit 0
+else
+  wget https://packages.microsoft.com/config/debian/13/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+    sudo dpkg -i packages-microsoft-prod.deb
+    rm packages-microsoft-prod.deb
+fi
+
+apt install -y dotnet-runtime-8.0
